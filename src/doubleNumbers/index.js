5601eb1aa6a5fc9a9fa0b2228ba8494c523134fc
nazzarkevich <nazzik.3@gmail.com>
2018-10-29 22:56:58 +0200

    added doubleNumbers and sort functions

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,14 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const length = num.toString().length;
+  const index = length / 2;
+
+  const firstPart = num.toString().slice(0, index);
+  const secondPart = num.toString().slice(index, length);
+
+  if (firstPart === secondPart) {
+    return num;
+  }
+
+  return num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,25 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const arrayCopy = arr.slice();
+
+  arrayCopy.sort(compareNumbers);
+
+  return arrayCopy;
+};
+
+const addNumbers = number => {
+  let result = null;
+
+  for (const num of number.toString()) {
+    result += parseInt(num);
+  }
+
+  return result;
+};
+
+const compareNumbers = (firstNumber, secondNumber) => {
+  const resultFirst = addNumbers(firstNumber);
+  const resultSecond = addNumbers(secondNumber);
+
+  return resultFirst - resultSecond;
 };
